8316e60cb14222ab2c00e40a1ddb85bead643fe5
Cristobal <cristobalz@outlook.com>
2019-06-12 21:35:09 -0400

    second commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,43 @@
 class TodosController < ApplicationController
+  before_action :todo_find, only: [:show, :edit, :update, :destroy]
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
+  def todo_find
+    @todo = Todo.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/todos', to: 'todos#index'
+  root 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create', as: 'create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  patch 'todos/:id', to: 'todos#update', as: 'update'
+  delete 'todos/:id', to: 'todos#destroy', as: 'delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
